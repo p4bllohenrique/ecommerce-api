793a8a7a392e3d584b049db8e4415a30f2d290e8
Pablo Silva <p4bllohenrique@gmail.com>
2025-11-25 20:50:43 -0300

    feat: create Order entity and OrderController; modify User entity

---
+++ src/main/java/com/pablohenrique/ecommerce/controllers/OrderController.java
@@ -0,0 +1,13 @@
+package com.pablohenrique.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/order")
+public class OrderController {
+
+
+
+}
+
