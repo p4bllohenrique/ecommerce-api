b498b67048ba777d66ab5f743d33bd63edcc4d43
Pablo Silva <p4bllohenrique@gmail.com>
2025-11-26 12:08:53 -0300

    feat: add OrderStatus enum and update Order and TestConfig

--- src/main/java/com/pablohenrique/ecommerce/config/TestConfig.java
+++ src/main/java/com/pablohenrique/ecommerce/config/TestConfig.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Profile;
 import java.time.Instant;
 import java.util.Arrays;
 
+import static com.pablohenrique.ecommerce.OrderStatus.*;
+
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
@@ -28,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria", "maria@gmail.com", "9999999999", "12345");
         User u2 = new User(null, "Joao", "joao@gmail.com", "8888888888", "12345");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), SHIPPED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
