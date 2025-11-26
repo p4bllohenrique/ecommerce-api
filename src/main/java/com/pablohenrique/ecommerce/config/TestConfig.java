69a4d8eaf4ce461d87200ea52d3811d536c05a47
Pablo Silva <p4bllohenrique@gmail.com>
2025-11-26 16:04:47 -0300

    feat: add Product entity, repository, service and controller; update Category and TestConfig

--- src/main/java/com/pablohenrique/ecommerce/config/TestConfig.java
+++ src/main/java/com/pablohenrique/ecommerce/config/TestConfig.java
@@ -2,9 +2,11 @@ package com.pablohenrique.ecommerce.config;
 
 import com.pablohenrique.ecommerce.entities.Category;
 import com.pablohenrique.ecommerce.entities.Order;
+import com.pablohenrique.ecommerce.entities.Product;
 import com.pablohenrique.ecommerce.entities.User;
 import com.pablohenrique.ecommerce.repositories.CategoryRepository;
 import com.pablohenrique.ecommerce.repositories.OrderRepository;
+import com.pablohenrique.ecommerce.repositories.ProductRepository;
 import com.pablohenrique.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -29,23 +31,33 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         User u1 = new User(null, "Maria", "maria@gmail.com", "9999999999", "12345");
         User u2 = new User(null, "Joao", "joao@gmail.com", "8888888888", "12345");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),WAITING_PAYMENT, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),PAID, u2);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), SHIPPED, u1);
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
     }
 
 }
--- src/main/java/com/pablohenrique/ecommerce/entities/Category.java
+++ src/main/java/com/pablohenrique/ecommerce/entities/Category.java
@@ -3,7 +3,7 @@ package com.pablohenrique.ecommerce.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_category")
@@ -12,7 +12,11 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
 
@@ -37,6 +41,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
